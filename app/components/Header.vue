256cc29b70e9582f188aacf12734f5554fd48b6f
Elena Kuzmitskaya <e.kuzmitskaya@gmail.com>
2025-10-13 01:26:09 +0300

    Урок 5

---
+++ app/components/Header.vue
@@ -0,0 +1,54 @@
+<template>
+    <header class="container header px-4 px-md-0">
+        <div class="row justify-content-between align-items-center">
+            <div class="col-lg-2 col-6">
+                <NuxtLink class="logo-link" to="/"> <img width="128" src="/images/logo.svg" alt="logo: Willberries"
+                        class="logo-image" />
+                </NuxtLink>
+
+            </div>
+            <div class="col-lg-6 d-none d-lg-block">
+                <nav>
+                    <ul class="navigation d-flex justify-content-around">
+                        <li class="navigation-item">
+                            <NuxtLink class="navigation-link" data-field="gender" to="/products">Women</NuxtLink>
+                        </li>
+                        <li class="navigation-item">
+                            <a href="#" class="navigation-link" data-field="gender">Men</a>
+                        </li>
+                        <li class="navigation-item">
+                            <a href="#" class="navigation-link" data-field="category">Clothing</a>
+                        </li>
+                        <li class="navigation-item">
+                            <a href="#" class="navigation-link" data-field="category">Accessories</a>
+                        </li>
+                        <li class="navigation-item">
+                            <a href="#" class="navigation-link" data-field="category">Shoes</a>
+                        </li>
+                        <li class="navigation-item">
+                            <a href="#" class="navigation-link">All</a>
+                        </li>
+
+                    </ul>
+                </nav>
+            </div>
+            <div class="col-lg-2 d-none d-lg-block">
+                <div class="form-control search-block">
+                    <input type="text" class="form-control" placeholder="Search" aria-label="Recipient's username"
+                        aria-describedby="button-addon2">
+                    <button class="btn btn-outline-secondary" type="button" id="button-addon2">
+                        <img src="/images/search.png" alt="search">
+                    </button>
+                </div>
+            </div>
+            <div class="col-lg-2 col-6 d-flex justify-content-end">
+                <button class="button button-cart">
+                    <img class="button-icon" src="/images/cart.svg" alt="icon: cart">
+                    <span class="button-text">Cart</span>
+                    <span class="button-text cart-count"></span>
+                </button>
+            </div>
+        </div>
+        <!-- /.row -->
+    </header>
+</template>
